c58bae15355d1c0a447317ec8b61b445d0e31a94
Munir Mahmud <munirmahmud6@gmail.com>
2021-12-30 15:03:53 +0600

    Food list for home page added

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,5 +1,4 @@
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './pages/About';
 import Home from './pages/Home';
 
 export default function App() {
@@ -7,7 +6,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
       </Routes>
     </Router>
   );
