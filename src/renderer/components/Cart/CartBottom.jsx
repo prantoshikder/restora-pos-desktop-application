be9e3ad9a47e96f9d976a23d15cfa408c765a614
Munir Mahmud <munirmahmud6@gmail.com>
2022-01-02 15:08:10 +0600

    Cart buttons added

--- src/renderer/components/Cart/CartBottom.jsx
+++ src/renderer/components/Cart/CartBottom.jsx
@@ -1,7 +1,27 @@
+import { Button } from 'antd';
 import React from 'react';
+import './cart.styles.scss';
 
 const CartBottom = () => {
-  return <div>CArt Bottom</div>;
+  const handleCancelCartItems = () => {};
+
+  const handleQuickOrder = () => {};
+
+  const handlePlaceOrder = () => {};
+
+  return (
+    <div className="btn-wrapper">
+      <Button type="primary" danger onClick={handleCancelCartItems}>
+        Cancel
+      </Button>
+      <Button type="primary" onClick={handleQuickOrder}>
+        Quick Ordedr
+      </Button>
+      <Button type="" className="light-blue" onClick={handlePlaceOrder}>
+        Place Order
+      </Button>
+    </div>
+  );
 };
 
 export default CartBottom;
