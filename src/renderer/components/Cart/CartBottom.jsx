9d6684a944357444cee496d578b37339ee1e3cbc
Munir Mahmud <munirmahmud6@gmail.com>
2022-01-03 13:44:08 +0600

    Cart buttons added

--- src/renderer/components/Cart/CartBottom.jsx
+++ src/renderer/components/Cart/CartBottom.jsx
@@ -1,3 +1,4 @@
+import { DeleteOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import React from 'react';
 import './cart.styles.scss';
@@ -10,17 +11,35 @@ const CartBottom = () => {
   const handlePlaceOrder = () => {};
 
   return (
-    <div className="btn-wrapper">
-      <Button type="primary" danger onClick={handleCancelCartItems}>
-        Cancel
-      </Button>
-      <Button type="primary" onClick={handleQuickOrder}>
-        Quick Ordedr
-      </Button>
-      <Button type="" className="light-blue" onClick={handlePlaceOrder}>
-        Place Order
-      </Button>
-    </div>
+    <>
+      <div className="grand-total mb-2">
+        <span>Grand Total</span>
+        <div>
+          <span>$</span>
+          <span>1876451.00</span>
+        </div>
+      </div>
+      <div className="btn-wrapper">
+        <Button
+          type="primary"
+          danger
+          onClick={handleCancelCartItems}
+          className="d-flex align-item-center justify-content-center"
+        >
+          <DeleteOutlined />
+        </Button>
+        <Button type="primary" onClick={handleQuickOrder}>
+          Quick Ordedr
+        </Button>
+        <Button
+          type="primary"
+          className="light-blue"
+          onClick={handlePlaceOrder}
+        >
+          Place Order
+        </Button>
+      </div>
+    </>
   );
 };
 
