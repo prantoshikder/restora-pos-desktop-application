111e237d673f6acfc5ba343014e9816d5410f0d6
Pranto Shikder <48595837+prantoshikder@users.noreply.github.com>
2022-01-16 15:49:58 -0800

    POS cart design

--- src/renderer/components/Cart/CartTop.jsx
+++ src/renderer/components/Cart/CartTop.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const CartTop = () => {
   return (
-    <div className="customer-detail">
-      <h1>Cart Top</h1>
+    <div className="form-content">
+      <div className="banner-card">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A, iure.
+          Earum commodi pariatur, ut est harum quaerat, obcaecati magnam ratione
+          sequi rem fuga? Eos veniam distinctio veritatis illo repellat tempore!
+        </p>
+        <Row className="search-food-wrapper justify-content-md-center">
+          <Col lg={8}>
+            <input type="text" placeholder="Search" className="form-control" />
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
